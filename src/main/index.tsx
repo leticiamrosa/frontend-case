97cbadd72e289be7e6200cd491f6a34236e0e233
leticiamrosa <leticiamonteiro.design@gmail.com>
2022-06-23 18:38:53 -0300

    chore: implementation the initial project

---
+++ src/main/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App: React.FC = () => {
+  return (
+        <div>
+            <h1>Initial Project</h1>
+        </div>
+  )
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('main')
+)
---
+++ webpack.dev.js
@@ -0,0 +1,46 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin')
+const { merge } = require('webpack-merge')
+const common = require('./webpack.common')
+const { DefinePlugin } = require('webpack')
+
+module.exports = merge(common, {
+  mode: 'development',
+  module: {
+    rules: [{
+      test: /\.ts(x?)$/,
+      loader: 'ts-loader',
+      exclude: /node_modules/
+    }, {
+      test: /\.scss$/,
+      use: [{
+        loader: 'style-loader'
+      }, {
+        loader: 'css-loader',
+        options: {
+          modules: true
+        }
+      }, {
+        loader: 'sass-loader'
+      }]
+    }]
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './public',
+    writeToDisk: true,
+    historyApiFallback: true,
+    port: 8080
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './template.dev.html'
+    }),
+    new FaviconsWebpackPlugin({
+      logo: './public/favicon/favicon-16x16.png'
+    }),
+    new DefinePlugin({
+      'process.env.API_URL': 'https://api-bank-nodejs.herokuapp.com/api/v1'
+    })
+  ]
+})
