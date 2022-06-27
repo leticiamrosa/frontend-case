4e1474c9e6d9ebc960b6dec846b7b9dcc7b0f155
leticiamrosa <leticiamonteiro.design@gmail.com>
2022-06-27 11:58:43 -0300

    fix: heroku port

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const path = require('path')
 const cors = require('cors')
 const app = express()
 
-const port = process.env.REACT_APP_PORT || 3000
+const port = process.env.PORT || 3000
 const root = path.join(__dirname, 'dist')
 
 app.use(cors())
