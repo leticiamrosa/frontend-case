630e8dc5db241fa280315716fc4d5c5a4fe4f6b0
leticiamrosa <leticiamonteiro.design@gmail.com>
2022-06-27 10:29:39 -0300

    chore: ajustment deploy

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,4 +1,4 @@
-const common = require('./webpack.config')
+const common = require('./webpack.common')
 const { merge } = require('webpack-merge')
 
 const HtmlWebpackPlugin = require('html-webpack-plugin')
@@ -8,36 +8,46 @@ const FaviconsWebpackPlugin = require('favicons-webpack-plugin')
 module.exports = merge(common, {
   mode: 'production',
   module: {
-    rules: [{
-      test: /\.ts(x?)$/,
-      loader: 'ts-loader',
-      exclude: /node_modules/
-    }, {
-      test: /\.scss$/,
-      use: [{
-        loader: MiniCssExtractPlugin.loader
-      }, {
-        loader: 'css-loader',
-        options: {
-          modules: true
-        }
-      }, {
-        loader: 'sass-loader'
-      }]
-    }, {
-      test: /\.svg/,
-      use: {
-        loader: 'svg-url-loader',
-        options: {
-          iesafe: true,
-          encoding: 'base64'
+    rules: [
+      {
+        test: /\.ts(x?)$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true
+            }
+          },
+          {
+            loader: 'sass-loader'
+          }
+        ]
+      },
+      {
+        test: /\.svg/,
+        use: {
+          loader: 'svg-url-loader',
+          options: {
+            iesafe: true,
+            encoding: 'base64'
+          }
         }
       }
-    }]
+    ]
   },
   externals: {
     react: 'React',
-    'react-dom': 'ReactDOM'
+    'react-dom': 'ReactDOM',
+    axios: 'axios',
+    'react-router-dom': 'ReactRouterDOM'
   },
   plugins: [
     new HtmlWebpackPlugin({
